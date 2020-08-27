6c9f6d483c396f70267a285d52ecfd1c25e50639
Aidan Carling <55260157+AidanCarling@users.noreply.github.com>
2020-08-27 18:37:03 +0100

    edits to contact form

--- js/contactform.js
+++ js/contactform.js
@@ -1,33 +1,38 @@
+const submit = document.getElementById('submitButton')
+
+submit.addEventListener('click', submitToAPI(e));
+
+
 function submitToAPI(e) {
     e.preventDefault();
     var URL = "https://4u8v6seemi.execute-api.eu-west-2.amazonaws.com";
 
          var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($("#name-input").val())) {
+         if (!Namere.test($(e.name).val())) {
                       alert ("Name can not less than 2 char");
              return;
          }
 
-         if ($("#email-input").val()=="") {
+         if ($(e.mail).val()=="") {
              alert ("Please enter your email id");
              return;
          }
 
-         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!reeamil.test($("#email-input").val())) {
+         var eamilre = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+         if (!eamilre.test($(e.mail).val())) {
              alert ("Please enter valid email address");
              return;
          }
 
-    var name = $("#name-input").val();
-    var email = $("#email-input").val();
-    var subject = $("#subject-input").val();
-    var desc = $("#description-input").val();
+    var namere = $(e.name).val();
+    var emailre = $(e.mail).val();
+    var subjectre = $(e.subject).val();
+    var descre = $(e.Message).val();
     var data = {
-       name : name,
-       email : email,
-       subject : subject,
-       desc : desc
+       name : namere,
+       email : emailre,
+       subject : subjectre,
+       desc : descre
      };
 
     $.ajax({
@@ -41,12 +46,12 @@ function submitToAPI(e) {
       
       success: function () {
         // clear form and show a success message
-        alert("Successfull");
-        document.getElementById("contact-form").reset();
+        alert("Successful");
+        document.getElementById(contact).reset();
     location.reload();
       },
       error: function () {
         // show an error message
-        alert("UnSuccessfull");
+        alert("Unsuccessful");
       }});
   }
---
+++ js/contactfunction.js
@@ -0,0 +1,13 @@
+/*console.log('starting function');
+
+const AWS = require('aws-sdk');
+
+exports.handle = function(e, ctx, callback) {
+    console.log(e);
+    var params = {
+        name: e.name,
+        email: e.mail,
+        subject: e.subject,
+        desc: e.message
+    };
+} */
