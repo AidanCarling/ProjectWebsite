51b051ed7316f090345b733c64440da19f04ba31
Aidan Carling <55260157+AidanCarling@users.noreply.github.com>
2020-08-29 21:53:44 +0100

    Fixing contact form submit

--- js/contactform.js
+++ js/contactform.js
@@ -1,57 +1,48 @@
-const submit = document.getElementById('submitButton')
+const form = document.getElementById('contactForm')
+const url = 'https://nk9apqaze8.execute-api.eu-west-2.amazonaws.com/dev/email/send'
+const toast = document.getElementById('toast')
+const submit = document.getElementById('submit')
 
-submit.addEventListener('click', submitToAPI(e));
+function post(url, body, callback) {
+  var req = new XMLHttpRequest();
+  req.open("POST", url, true);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.addEventListener("load", function () {
+    if (req.status < 400) {
+      callback(null, JSON.parse(req.responseText));
+    } else {
+      callback(new Error("Request failed: " + req.statusText));
+    }
+  });
+  req.send(JSON.stringify(body));
+}
+function success () {
+  toast.innerHTML = 'Thanks for sending me a message! I\'ll get in touch with you ASAP.'
+  submit.disabled = false
+  submit.blur()
+  form.name.focus()
+  form.name.value = ''
+  form.email.value = ''
+  form.content.value = ''
+}
+function error (err) {
+  toast.innerHTML = 'There was an error with sending your message.'
+  submit.disabled = false
+  console.log(err)
+}
 
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  toast.innerHTML = 'Sending'
+  submit.disabled = true
 
-function submitToAPI(e) {
-    e.preventDefault();
-    var URL = "https://4u8v6seemi.execute-api.eu-west-2.amazonaws.com";
-
-         var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($(e.name).val())) {
-                      alert ("Name can not less than 2 char");
-             return;
-         }
-
-         if ($(e.mail).val()=="") {
-             alert ("Please enter your email id");
-             return;
-         }
-
-         var eamilre = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!eamilre.test($(e.mail).val())) {
-             alert ("Please enter valid email address");
-             return;
-         }
-
-    var namere = $(e.name).val();
-    var emailre = $(e.mail).val();
-    var subjectre = $(e.subject).val();
-    var descre = $(e.Message).val();
-    var data = {
-       name : namere,
-       email : emailre,
-       subject : subjectre,
-       desc : descre
-     };
-
-    $.ajax({
-      type: "POST",
-      url : "https://4u8v6seemi.execute-api.eu-west-2.amazonaws.com",
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-
-      
-      success: function () {
-        // clear form and show a success message
-        alert("Successful");
-        document.getElementById(contact).reset();
-    location.reload();
-      },
-      error: function () {
-        // show an error message
-        alert("Unsuccessful");
-      }});
+  const payload = {
+    name: form.name.value,
+    email: form.email.value,
+    content: form.content.value
   }
+  post("https://nk9apqaze8.execute-api.eu-west-2.amazonaws.com/dev/email/send", payload, function (err, res) {
+    if (err) { return error(err) }
+    success()
+  })
+})
